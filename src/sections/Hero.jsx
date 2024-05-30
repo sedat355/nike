8df07e52badbd9cb837ed254cbf6aaf4522adb4e
Sedat Polat <sedatpolat355@gmail.com>
2024-05-30 18:26:59 +0300

    hero text kısmı ok.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero px-8 py-3 bg-pink-50'>
+      <div>
+        <p>
+          Our Summer collection
+        </p>
+        <h1 className='text-[42px] font-bold font-palanquin'>
+          <span className=''>The New Arrival</span>
+          <br />
+          <span className='text-orange-600 mr-3'>Nike</span>
+          Shoes
+        </h1>
+        <p className='text-slate-gray text-xs max-w-72'>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+
+        <button className='bg-orange-500 text-white px-4 py-1 mt-6 rounded font-bold'>Shop now</button>
+
+        <div className='flex space-x-4 mt-3'>
+          {statistics.map(stat => {
+            return(
+              <div key={stat.value} className='flex flex-col'>
+                <span className='text-[22px] font-bold'>{stat.value}</span>
+                <span className='text-xs text-slate-gray'>{stat.label}</span>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+      
+    </section>
   )
 }
 
